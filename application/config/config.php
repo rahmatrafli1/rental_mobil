8f299e8d6bea31213020d64d766fade51d566094
rahmatrafli1 <rahmatrafli1@gmail.com>
2023-06-20 14:32:56 +0700

    mengubah isi dari file system

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CI_Project/rental_mobil/';
+$config['base_url'] = 'https://rentalmobil.crowndfunding-rahmat-rafli.com/';
 
 /*
 |--------------------------------------------------------------------------
